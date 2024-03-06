8596476f958fa26605ab10a0098a582086b097f1
Julio Carmo <jcarmomg@gmail.com>
2024-03-05 22:51:58 -0300

    Initial Commit

---
+++ src/main/java/br/cnj/java_pdpj_2024/JavaPdpj2024Application.java
@@ -0,0 +1,13 @@
+package br.cnj.java_pdpj_2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPdpj2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaPdpj2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/cnj/java_pdpj_2024/JavaPdpj2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.cnj.java_pdpj_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPdpj2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
